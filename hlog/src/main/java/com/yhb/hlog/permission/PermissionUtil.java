2a7658d544822a8fd9651e6935a4000fc5b69026
YeHaobo <17855366369@163.com>
2023-12-21 15:31:38 +0800

    优化权限判断2023.12.21

--- hlog/src/main/java/com/yhb/hlog/permission/PermissionUtil.java
+++ hlog/src/main/java/com/yhb/hlog/permission/PermissionUtil.java
@@ -3,7 +3,7 @@ package com.yhb.hlog.permission;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
 import android.util.Log;
 import com.yhb.hlog.expose.HLog;
 
@@ -19,7 +19,7 @@ public class PermissionUtil {
     /**判断是否缺少权限*/
     public static boolean hasPermission(Context context) {
         for (String permission : permissions) {
-            if (ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_DENIED) {
+            if (PermissionChecker.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
                 Log.e(HLog.TAG,"No read/write permissions for files");
                 return false;
             }
