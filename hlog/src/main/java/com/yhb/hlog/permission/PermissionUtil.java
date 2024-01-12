819934e4ebb624ec54628e0ae9aaacb281a0433f
YeHaobo <17855366369@163.com>
2024-01-12 14:54:16 +0800

    恢复权限判断2024.01.12

--- hlog/src/main/java/com/yhb/hlog/permission/PermissionUtil.java
+++ hlog/src/main/java/com/yhb/hlog/permission/PermissionUtil.java
@@ -3,7 +3,7 @@ package com.yhb.hlog.permission;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.support.v4.content.PermissionChecker;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import com.yhb.hlog.expose.HLog;
 
@@ -19,7 +19,7 @@ public class PermissionUtil {
     /**判断是否缺少权限*/
     public static boolean hasPermission(Context context) {
         for (String permission : permissions) {
-            if (PermissionChecker.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_DENIED) {
                 Log.e(HLog.TAG,"No read/write permissions for files");
                 return false;
             }
